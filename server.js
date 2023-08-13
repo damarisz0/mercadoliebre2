b43d4cf1072b572a535b112b6b8fa09a87c06459
zoe amador <zoeamador.234@gmail.com>
2023-08-13 20:52:03 -0300

    subiendo proyecto mercado liebre

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app= express()
 app.use(express.static('public'))
 app.get('/',(req ,res)=>{
     res.sendFile(__dirname + '/views/index.html')
+
+})
+app.get('/login', (req, res)=>{
+    res.sendFile(__dirname + '/views/login.html')
+    
+});
+app.get('/register', (req, res)=>{
+    res.sendFile(__dirname + '/views/register.html')
 })
 app.listen(8080,()=>{
     console.log('servidor corriendo')
